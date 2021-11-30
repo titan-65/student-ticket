9a9b606cc3c0e309f2d85f9caff11dea9674b0d7
Vantol Bennett <vvantol2000@icloud.com>
2021-11-30 10:24:50 -0500

    Update index.js

--- src/state/index.js
+++ src/state/index.js
@@ -3,24 +3,33 @@ import { supabase } from '../supabase'
 
 // Create a new store instance.
 const store = createStore({
-    state: {
-      user: {},
-      tickets: [],
+  state: {
+    user: {},
+    currentUser: null,
+    tickets: [],
   },
   mutations: {
     SET_USER(state, user) {
       state.user = user
-    }
+    },
+    ADD_USER(state, user) {
+      state.currentUser = user
+    },
   },
   getters: {
-    user: state => state.user
+    user: (state) => state.user,
+    tickets: (state) => state.tickets,
+    currentUser: (state) => state.currentUser,
   },
   actions: {
     async fetchUser({ commit }) {
       const user = await supabase.auth.user()
- 
+
       commit('SET_USER', user)
-    }
+    },
+    async addUser({ commit }, user) {
+      commt('ADD_USER', user)
+    },
   },
 })
 
