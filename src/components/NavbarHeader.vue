9e9d5de0680ab1cbd4f7da76f7f16b26f3542a95
Vantol Bennett <vvantol2000@icloud.com>
2021-11-30 11:49:46 -0500

    Update NavbarHeader.vue

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -6,7 +6,7 @@
           <!-- <img
             src="https://res.cloudinary.com/zhyjenae/image/upload/v1638055663/signature_q07em0.png"
           /> -->
-		  STUCHECK
+          STUCHECK
         </router-link>
 
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
@@ -15,18 +15,16 @@
           <span aria-hidden="true"></span>
         </a>
       </div>
-	  <div class="navbar-start" v-if="user">
-		  <router-link :to="{ name: 'TicketsPage' }" class="navbar-item">
+      <div class="navbar-start" v-if="user">
+        <router-link :to="{ name: 'TicketsPage' }" class="navbar-item">
           Tickets
         </router-link>
-	  </div>
+      </div>
       <div class="navbar-end">
         <router-link :to="{ name: 'Home' }" active-class="active" class="navbar-item"
           >Student</router-link
         >
-        <router-link :to="{ name: 'PublicFaq' }" class="navbar-item">
-          FAQ
-        </router-link>
+        <router-link :to="{ name: 'PublicFaq' }" class="navbar-item"> FAQ </router-link>
         <router-link :to="{ name: 'Login' }" class="navbar-item" v-if="!user">
           Login
         </router-link>
@@ -37,7 +35,7 @@
             <router-link :to="{ name: 'Home' }" class="navbar-item">
               {{ user.email }}
             </router-link>
-			<a class="button navbar-item is-light" @click="logout">Signout</a>
+            <a class="button navbar-item is-light" @click="logout">Signout</a>
           </div>
         </div>
       </div>
@@ -46,29 +44,30 @@
 </template>
 
 <script>
+//TODO: add a watcher that watches changes when user login changes
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { computed, ref } from 'vue'
 import { supabase } from '../supabase.js'
 export default {
   setup() {
-	  const router = useRouter()
+    const router = useRouter()
     const isActive = ref(true)
     const store = useStore()
-	store.dispatch('fetchUser')
+    store.dispatch('fetchUser')
     const user = computed(() => {
       return store.state.user
     })
-	
-	const logout = async () => {
-		let { error } = await supabase.auth.signOut()
-		if (error) throw error
-		router.push('/')
-	}
+
+    const logout = async () => {
+      let { error } = await supabase.auth.signOut()
+      if (error) throw error
+      router.push('/')
+    }
     return {
       user,
       isActive,
-	  logout
+      logout,
     }
   },
 }
@@ -82,10 +81,9 @@ export default {
   opacity: 0.8;
 }
 
-
 @media screen and (min-width: 1024px) {
   .bd-navbar {
-	  padding: 1rem 2rem
+    padding: 1rem 2rem;
   }
 }
 </style>
