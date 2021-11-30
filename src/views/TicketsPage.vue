0bfbcc8e731f6d560aac4f85ee76b919aa2941e8
Vantol Bennett <vvantol2000@icloud.com>
2021-11-30 00:02:52 -0500

    Update TicketsPage.vue

--- src/views/TicketsPage.vue
+++ src/views/TicketsPage.vue
@@ -1,4 +1,23 @@
 <template>
+	<section class="hero is-medium">
+		<div class="container">
+			<div class="hero-body">
+				<h1 class="title">Tickets</h1>
+			</div>
+		</div>
+	</section>
+	<section class="section">
+		<div class="container">
+			<h1>Current Tickets</h1>
+			<div class="tickets">
+				<Loading v-if="!ticketData"/>
+				<div class="current-tickets" v-else>
+					
+				</div>
+			</div>
+		</div>
+		
+	</section>
 </template>
 
 <script>
