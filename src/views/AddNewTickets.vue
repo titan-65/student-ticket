d21007d81f2689e53c9a5e0a30a8f08db9f1d074
Vantol Bennett <vvantol2000@icloud.com>
2021-12-01 19:47:54 -0500

    Update AddNewTickets.vue

--- src/views/AddNewTickets.vue
+++ src/views/AddNewTickets.vue
@@ -1,8 +1,28 @@
 <template>
+  <SmartForm> : </SmartForm>
 </template>
 
 <script>
+import SmartForm from '../components/SmartForm.vue'
+
+import { ref, computed } from 'vue'
+export default {
+  components: {
+    SmartForm,
+  },
+  setup() {
+    const name = ref('')
+    const category = ref('')
+    const valid = computed(() => {
+      return !name && !category
+    })
+    return {
+      name,
+      category,
+      valid,
+    }
+  },
+}
 </script>
 
-<style>
-</style>
+<style></style>
